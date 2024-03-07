386cd7512ba94fe8de52a3756f139356c6254cb8
Josh Sinclair <j.sinclairthomson@gmail.com>
2024-03-07 08:47:48 +0000

    add model to admin.py and create forms.py and add VenueForm class

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,13 @@
 
 from django.contrib import admin
 from .models import Events
+from .models import Venue
 
 # Register your models here.
 admin.site.register(Events)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+	list_display = ('name', 'address', 'phone')
+	ordering = ('name',)
+	search_fields = ('name', 'address')
