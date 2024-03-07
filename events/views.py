818b820d62289c407ee75035aae1b6b0cae98d1b
Josh Sinclair <j.sinclairthomson@gmail.com>
2024-03-07 09:12:58 +0000

    add calendar views to index.html

--- events/views.py
+++ events/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+from django.http import HttpResponseRedirect
+from .models import Events, Venue
+from django.contrib.auth.models import User
+from .forms import VenueForm
+from django.contrib import messages
+from django.core.paginator import Paginator
+
 
 # Create your views here.
-def index(request):
-    return render(request, 'events/index.html')
+def index(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = request.user
+    month = month.capitalize()
+# Convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number - int(month_number)
+# create a calendar
+    cal = HTMLCalendar().formatmonth(
+		year, 
+		month_number)
+# Get current year
+    now = datetime.now() 
+    current_year = now.year
+#Query the Events Model For Dates
+    # event_list = Event.objects.filter(
+	# 	event_date__year = year,
+	# 	event_date__month = month_number,
+	# 	)
+# Get current time
+    time = now.strftime('%H:%M:%S %p')
+
+    return render(request, 'events/index.html', {
+		'name': name,
+		'year': year,
+		'month': month,
+		'month_number': month_number,
+		'cal': cal,
+		'current_year': current_year,
+		'time' : time,
+		# 'event_list': event_list,
+		})
+
