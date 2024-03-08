111834551601984596d365b4da043e84684cfd65
Josh Sinclair <j.sinclairthomson@gmail.com>
2024-03-08 11:25:04 +0000

    create views and urls

--- review/views.py
+++ review/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Review
+from .forms import ReviewForm, CommentForm
 
-# Create your views here.
+def review_list(request):
+    reviews = Review.objects.all()
+    return render(request, 'yourapp/review_list.html', {'reviews': reviews})
+
+def review_detail(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.review = review
+            comment.author = request.user
+            comment.save()
+            return redirect('review_detail', pk=review.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'yourapp/review_detail.html', {'review': review, 'form': form})
