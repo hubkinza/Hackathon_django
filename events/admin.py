5592ea9f4db6c43f854cedbcf318bfb9491fc038
Josh Sinclair <j.sinclairthomson@gmail.com>
2024-03-08 13:02:35 +0000

    add review and comment display and comment add feature on event page

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from .models import Events, Venue, Event
+from .models import Events, Venue, Event, Review, Comment
 
 # Register your models here.
 admin.site.register(Events)
@@ -17,3 +17,17 @@ class EventAdmin(admin.ModelAdmin):
 	list_display = ('name', 'event_date', 'venue')
 	list_filter = ('event_date', 'venue')
 	ordering = ('event_date',)
+
+@admin.register(Review)
+class EventAdmin(admin.ModelAdmin):
+	fields = ('title', 'content', 'author')
+	list_display = ('title', 'content', 'author')
+	list_filter = ('title', 'content', 'author')
+	ordering = ('created_at',)
+
+@admin.register(Comment)
+class EventAdmin(admin.ModelAdmin):
+	fields = ('review', 'author', 'content')
+	list_display = ('review', 'author', 'content')
+	list_filter = ('review', 'author', 'content')
+	ordering = ('created_at',)
